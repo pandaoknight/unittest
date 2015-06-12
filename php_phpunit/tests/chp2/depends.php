3755148ecf09f86f6f87061787470db05d0c1078
pandaoknight@iZ25tw9lbzeZ <pandaoknight@iZ25tw9lbzeZ.com>
2015-06-12 10:54:47 +0800

    +[实现] 完成phpunit 4.7.2的 @depends标注测试

--- php_phpunit/tests/chp2/depends.php
+++ php_phpunit/tests/chp2/depends.php
@@ -2,6 +2,28 @@
 
 class ch2_depends extends PHPUnit_Framework_TestCase{
     public function testFirst(){
-        print "hello";
+        $stack = array();
+        $stack[] = "first";
+        return $stack;
+    }
+
+    /**
+     * @depends testFirst
+     */
+    public function testSecond($stack){
+        $this->assertEquals(1, sizeof($stack));   // 在减少重复计算的过程中，其实也会形成test之间的耦合。
+        $stack[] = "second";
+        return $stack;
+    }
+
+    /**
+     * @depends testFirst
+     * @depends testSecond
+     */
+    public function testThird($first, $second){
+        var_dump($first);
+        var_dump($second);
+        $this->assertEquals("first", $first[0]);
+        $this->assertEquals(2, sizeof($second));
     }
 }
